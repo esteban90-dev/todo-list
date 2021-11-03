e3ed1517c8254b1d6920b49035c0ce0df478a524
Stephen Allard <stephencallard@gmail.com>
2021-11-03 14:21:01 -0500

    add todo factory

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("hello worldz");
+import Todo from './todo-factory.js';
+
+var todo1 = Todo('take out the trash','take the trash to the garbage can','today','low');
+todo1.setTitle('go get groceries');
+console.log(todo1.getTitle());
+
+todo1.setDescription('go to whole foods');
+console.log(todo1.getDescription());
+
+todo1.setDueDate('tomorrow');
+console.log(todo1.getDueDate());
+
+todo1.setPriority('medium');
+console.log(todo1.getPriority());
+
+todo1.setComplete();
+console.log(todo1.getIsComplete());
+
+todo1.setIncomplete();
+console.log(todo1.getIsComplete());
---
+++ src/todo-factory.js
@@ -0,0 +1,55 @@
+function Todo(title, description, dueDate, priority){
+  var title = title;
+  var description = description;
+  var dueDate = dueDate;
+  var priority = priority;
+  var isComplete = false;
+
+  function getTitle(){
+    return title;
+  }
+
+  function setTitle(newTitle){
+    title = newTitle;
+  }
+
+  function getDescription(){
+    return description;
+  }
+
+  function setDescription(newDescription){
+    description = newDescription;
+  }
+
+  function getDueDate(){
+    return dueDate;
+  }
+
+  function setDueDate(newDueDate){
+    dueDate = newDueDate;
+  }
+
+  function getPriority(){
+    return priority;
+  }
+
+  function setPriority(newPriority){
+    priority = newPriority;
+  }
+
+  function getIsComplete(){
+    return isComplete;
+  }
+
+  function setComplete(){
+    isComplete = true;
+  }
+
+  function setIncomplete(){
+    isComplete = false;
+  }
+
+  return { getTitle, setTitle, getDescription, setDescription, getDueDate, setDueDate, getPriority, setPriority, getIsComplete, setComplete, setIncomplete };
+}
+
+export default Todo;

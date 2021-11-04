bfeed4f4329715db811cdb3a5702120cba406275
Stephen Allard <stephencallard@gmail.com>
2021-11-04 10:32:22 -0500

    update comments

--- src/todo-controller.js
+++ src/todo-controller.js
@@ -10,37 +10,46 @@ const TodoController = (function(){
   const todoShowView = TodoShowView;
 
   function index(){
+    //render todos
     todoIndexView.render(todoModel.getTodos());
 
-    //bind new button event to the 'new' action
+    //bind new button event to the 'new' method
     todoIndexView.handleClickNew(neW);
 
-    //if there are any todos on the index view, bind their 'show' events to the 'show' action
+    //if there are any todos on the index view, bind their 'show' events to the 'show' method
     if (todoModel.getTodos()){
       todoIndexView.handleClickShow(show);
     }
   }
 
   function neW(){
+    //render the new todo page
     todoNewView.render();
 
-    //bind form submission on the new view to the 'create' action
+    //bind form submission on the new view to the 'create' method
     todoNewView.handleFormSubmit(create);
   }
 
   function create(event){
+    //prevent page refresh after form submission
     event.preventDefault();
+
+    //gather submitted form data
     const formData = new FormData(event.target);
     const title = formData.get('title');
     const description = formData.get('description');
     const dueDate = formData.get('dueDate');
     const priority = formData.get('priority');
 
+    //create new todo
     TodoModel.create(title, description, dueDate, priority);
+
+    //render the index page
     index();
   }
 
   function show(event){
+    //render the show page for the given todo
     todoShowView.render(todoModel.read(event.target.getAttribute("id")));
   }
 

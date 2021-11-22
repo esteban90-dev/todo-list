6c7df5eb4cf6eb11d7495ec62eaa4fc20c4ab90d
Stephen Allard <stephencallard@gmail.com>
2021-11-22 09:27:57 -0600

    display 'no projects yet' on the project index view if there are no projects

--- src/project-index-view.js
+++ src/project-index-view.js
@@ -9,37 +9,44 @@ const ProjectIndexView = (function(){
     clear(app); 
 
     //display projects
-    for(let i=0; i<projects.length; i++){
-      //create title element
-      let a1 = document.createElement("a");
-      a1.setAttribute("data-project-id",projects[i].getId());
-      a1.setAttribute("href","#")
-      a1.innerHTML = projects[i].getTitle();
-      a1.setAttribute("id","show");
-
-      //create description element
-      let p = document.createElement("p");
-      p.innerHTML = projects[i].getDescription();
-
-      //create project edit link
-      let a2 = document.createElement("a");
-      a2.setAttribute("data-project-id",projects[i].getId());
-      a2.setAttribute("href","#");
-      a2.setAttribute("id","edit");
-      a2.innerHTML = "edit";
-
-      //create project delete link
-      let a3 = document.createElement("a");
-      a3.setAttribute("data-project-id",projects[i].getId());
-      a3.setAttribute("href","#");
-      a3.setAttribute("id","delete");
-      a3.innerHTML = "delete";
-
-      //append to app element
-      app.appendChild(a1);
+    if(projects.length === 0){
+      //display 'no new projects'
+      const p = document.createElement('p');
+      p.innerHTML = "no projects yet";
       app.appendChild(p);
-      app.appendChild(a2);
-      app.appendChild(a3);
+    } else {
+      for(let i=0; i<projects.length; i++){
+        //create title element
+        let a1 = document.createElement("a");
+        a1.setAttribute("data-project-id",projects[i].getId());
+        a1.setAttribute("href","#")
+        a1.innerHTML = projects[i].getTitle();
+        a1.setAttribute("id","show");
+  
+        //create description element
+        let p = document.createElement("p");
+        p.innerHTML = projects[i].getDescription();
+  
+        //create project edit link
+        let a2 = document.createElement("a");
+        a2.setAttribute("data-project-id",projects[i].getId());
+        a2.setAttribute("href","#");
+        a2.setAttribute("id","edit");
+        a2.innerHTML = "edit";
+  
+        //create project delete link
+        let a3 = document.createElement("a");
+        a3.setAttribute("data-project-id",projects[i].getId());
+        a3.setAttribute("href","#");
+        a3.setAttribute("id","delete");
+        a3.innerHTML = "delete";
+  
+        //append to app element
+        app.appendChild(a1);
+        app.appendChild(p);
+        app.appendChild(a2);
+        app.appendChild(a3);
+      }
     }
 
     //display new project button

bd5c55d5137e71ddbac360f56003a1a558e45221
Stephen Allard <stephencallard@gmail.com>
2021-12-01 20:33:02 -0600

    load project index when page loads

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 import ProjectController from './project-controller.js';
 import TodoController from './todo-controller.js';
-import ProjectModel from './project-model.js';
-import Storage from './storage.js';
 
 //initialize controllers
 ProjectController.initialize();
 TodoController.initialize();
 
-//when the page first loads, display the first project if projects are available,
-//otherwise display the project index
+//when the page first loads, display the project index
+ProjectController.index();
 
-if(ProjectModel.getProjects().length > 0){
-  ProjectController.show(1);
-} else {
-  ProjectController.index();
-}
 
 
 
